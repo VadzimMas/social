d637ca6395b8e1261ded1eb9477592253bd6ec06
VM-Web <mas.usa.gov@gmail.com>
2022-11-22 12:20:02 -0500

    q

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import Container from './components/container/Container';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Container />
+  </React.StrictMode>
+);
+
+
+
