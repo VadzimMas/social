312efd6a428a2e34cbc595f28dd2a6adf13223f9
VM-Web <mas.usa.gov@gmail.com>
2022-11-23 07:07:33 -0500

    q

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -18,11 +18,6 @@ const Profile = () => {
             />
             <MyPost />
             <Messages
-                likeCount='5'
-                dislikeCount='2'
-                message='lorem'
-            />
-            <Messages
                 message='The idea behind -webkit-fill-available  at least at one point  was to allow for an element to intrinsically fit into a particular layout, i.e., fill the available space for that property. At the moment intrinsic values like this fully supported by the'
             />
             <Messages
