6af08cd252ef586772d5639ed556661618ca7771
VM-Web <mas.usa.gov@gmail.com>
2022-11-23 08:03:29 -0500

    adaptive vh

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -3,7 +3,13 @@ import Aside from '../aside/Aside.jsx'
 import Main from '../main/Main.jsx';
 import styles from './container.module.scss'
 
+const setVh = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
 
+window.addEventListener('load', setVh);
+window.addEventListener('resize', setVh);
 
 
 const Container = () => {
